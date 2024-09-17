c02f994a822758b1c52e4dc36dd064498274f21e
Vladimir Drugov <wolffdrugov@yandex.ru>
2024-09-18 01:44:54 +0300

    mater:im rly stupid

--- 2023/day1/part1/sol.go
+++ 2023/day1/part1/sol.go
@@ -4,24 +4,48 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	input, err := os.ReadFile("input.txt")
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	lines := strings.Split(string(input[:]), "\n")
-	results := []rune{}
+
+	lines := strings.Split(string(input), "\n")
+	var total int64
 	for i := 0; i < len(lines); i++ {
-        numbers := []int {};
+		numbers := []int64{}
+
 		for _, char := range lines[i] {
-			number := int(char - '0')
-            numbers = append(numbers, number);
-            results = append(results, rune(numbers[:1][0]) + rune(numbers[len(numbers)-1:][0]) + '\n')
+			num, err := strconv.ParseInt(string(char), 10, 0) // base 10, auto-detect bit size
+			fmt.Println(num, err, " num err")
+			if err == nil {
+				numbers = append(numbers, num)
+			}
 		}
-        numbers = []int{};
-        fmt.Print(results)
+
+		if len(numbers) <= 0 {
+			continue
+		}
+
+		var newNumber string
+		for _, valNum := range numbers {
+			newNumber += strconv.FormatInt(valNum, 10)
+		}
+		fmt.Println(newNumber, " new nubjer")
+		newNumberInt, err := strconv.Atoi(newNumber)
+		fmt.Println(newNumberInt, " new number int")
+		if err != nil {
+			panic("are you that dumb?")
+		}
+
+		total += int64(newNumberInt)
+
+		numbers = []int64{}
 	}
+	fmt.Println(total, " total number")
 }
